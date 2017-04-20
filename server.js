a83554a80709a41de31c7e3c9842894ea96354d4
Reagan Wood <reaganwood1@gmail.com>
2017-04-20 14:17:23 -0500

    added class code

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 
 // Constants
 const PORT = 3000;
+module.export('authCAS'); // this isn't right I think
 
 // Requires
 var fs = require('fs');
@@ -13,6 +14,7 @@ var express = require('express');
 // The Express app
 var app = express();
 
+var server = https
 // Serve files from public folder
 app.use(express.static('public'));
 
